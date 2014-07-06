de9ec9bb8878c6dd145db079fad9dcd1c7a09baf
Courtney Campany <courtneycampany@gmail.com>
2014-07-06 15:57:40 +1000

    this script now can caclualte delta and xsi from tdl chamber flux...i thing the sample CO2 should be replaced with the chamberflux co2 to calc totco2 and xsi

--- scripts/cham_flux.R
+++ scripts/cham_flux.R
@@ -1,60 +1,91 @@
 require(plotBy)
 require(lubridate)
 require(plyr)
+library(reshape)
 
 #read in tdl summary 
 tdl_sites <- read.csv("raw data/flux_sites.csv")
 tdl_sites$month <- as.character(tdl_sites$month)
 tdl_sites$id <- paste(tdl_sites$month, tdl_sites$day, sep="-")
+tdl_sites$chamber <- gsub("ref", 99, tdl_sites$chamber)
 
 #tdl chamber flux--------------------------------------------------------------------------------------------
 tdl_format_func <- function(x) { 
-                          c(x$Date <- as.Date(x$Date), 
+                          c(x$Date <- dmy(x$Date), 
                           x$time <- as.POSIXct(paste(x$Date, x$time), format="%Y-%m-%d %H:%M:%S %p"),
                           x$day <- gsub("day_", "", x$day),
                           x$month <- tolower(month(x$Date, label=TRUE, abbr=FALSE)),
-                          x$id <- paste(x$month, x$day, sep="-"))
-                          return(x)
+                          x$id <- paste(x$month, x$day, sep="-"),
+                          nr<- nrow(x)/26,
+                          x$run <- rep(1:nr, each=26),
+                          x$co2_tot <- ((x$CorrConcA_Avg+x$CorrConcB_Avg)/(1-.00474)),
+                          dfr <- subset(x, SiteOutput != c("3", "4")),
+                          nr2<- nrow(dfr)/12,
+                          dfr$innerrun <- rep(1:nr2, each=12))
+                          return(dfr)
 }
 
 
 #read in summary files for all volumes into a list
 chamfluxs <- list.files(path = "raw data/chamber_flux/", pattern="^chflux", full.names = TRUE)
-chflux_list <- lapply(chamfluxs, function(x) read.csv(x))
+chflux_list <- lapply(chamfluxs, function(x) {a <- read.csv(x)
+                                             a<- a[complete.cases(a),]
+                                              droplevels(a)})
 
+
+#sapply(chflux_list,  nrow)/26
 #test formating 
-# test <- as.data.frame(chflux_list[5])
-# a <- tdl_format_func(test)
+#test2 <- as.data.frame(chflux_list[[11]])
+#a <- tdl_format_func(test2)
 
-#no format each lost
+#run tdl format
 chflux_ls <- llply(chflux_list, function(x) tdl_format_func(x))
-# a<-as.data.frame(chflux_ls[4])
-# str(chflux_list[4])
+
+test2 <- chflux_ls[[4]]
 
 # merges with sites 
+#######doesnt merge chambers properly (removed ref gases, sites 3and 4 to make it work)
 tdlchams <- llply(chflux_ls, function(x) join(x, tdl_sites[,c(3:5,7)], by=c("id", "SiteOutput"), type="left"))
-#b<- as.data.frame(tdlchams[3])
+#test <- as.data.frame(tdlchams[[8]])
+#write.csv(test, "testtdl.csv", row.names=FALSE)
 
-##function that takes each cm list, calculates xsi, 
-xsi_function <- function(x){
-  x$co2_tot <- (x$CorrConcA_Avg+x$CorrConcB_Avg)/(1-.00474)
-  x_sp <- split(x, ref_samp)
-  
-}
-
-###ddply to split dfr by chamber
-###if diff betwen ref and sample then use diff  (will inherently divide a-b)
-ddply(data, .(chamber), diff(variable))
+test <- tdlchams[[4]]
+xsidfr <- cast(test, id + run + chamber + innerrun~ ref_samp, value = "co2_tot")
 
 
-#for more complicated then make a simple function
-
+########calculate gm parameters dsamp-dref, xsi, big D
 xsifunc <- function(x){
-  a <- with(x, CorrConcA_Avg[ref_samp=="a"] )
-  b <- with(x, CorrConcA_Avg[ref_samp=="b"] )
-  xsi <- a/(a-b)
-  
+  xsidfr <- cast(x, run + id+chamber + innerrun~ ref_samp, value = "co2_tot")
+  xsidfr$xsi <- with(xsidfr, b/(b-a))
+  return(xsidfr)
 }
+  
+deltafunc <- function(x){
+  deltadfr <- cast(x, time+run + chamber + innerrun~ ref_samp, value = "Corrdel13C_Avg")
+  deltadfr$delta <- with(deltadfr, b-a)
+  return(deltadfr)
+}
+
+
+xsi_calc <- lapply(tdlchams, function(x) xsifunc(x))
+delta_calc <- lapply(tdlchams, function(x) deltafunc(x))
+
+t <- xsi_calc[[3]]
+
+#merge these two drs and then calculate DELTA
+###will require delta13sample*******
+
+####should I use the CO2 from chamber flux with CO2 from ambient to calculate xsi?  Use the two tdl deltas
+#for delta c13?
+
+
+
+# xsifunc <- function(x){
+#   a <- with(x, CorrConcA_Avg[ref_samp=="a"] )
+#   b <- with(x, CorrConcA_Avg[ref_samp=="b"] )
+#   xsi <- a/(a-b)
+#   
+# }
 
 
 ####ref a and b needed for xsi and delta calcs
